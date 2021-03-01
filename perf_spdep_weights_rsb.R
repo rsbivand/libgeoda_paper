19f2d1f1457414c0baf51a09212bd4b3deba55e4
Roger Bivand <roger.bivand@nhh.no>
2021-03-01 14:57:58 +0100

    update to latest spdep

--- perf_spdep_weights_rsb.R
+++ perf_spdep_weights_rsb.R
@@ -17,18 +17,21 @@ CreateWeights <- function(file_path, dt){
     w <- nb2listw(nb, style="W")
   } else {
 # using an sf equivalent of rgeos::gUnarySTRtreeQuery()
-    fB1 <- st_intersects(st_as_sfc(lapply(st_geometry(dt),
-        function(x) {st_as_sfc(st_bbox(x))[[1]]})))
-    fB1a <- lapply(seq_along(fB1), function(i) fB1[[i]][fB1[[i]] > i])
-    fB1a <- fB1a[-length(fB1a)]
-    nb_q <- poly2nb(dt, queen=TRUE, foundInBox=fB1a)
+#    fB1 <- st_intersects(st_as_sfc(lapply(st_geometry(dt),
+#        function(x) {st_as_sfc(st_bbox(x))[[1]]})))
+#    fB1a <- lapply(seq_along(fB1), function(i) fB1[[i]][fB1[[i]] > i])
+#    fB1a <- fB1a[-length(fB1a)]
+#    nb_q <- poly2nb(dt, queen=TRUE, foundInBox=fB1a)
+# this code now inside poly2nb() and used if foundInBox=NULL and geometry
+# count >= 500 (https://github.com/r-spatial/spdep/commit/ef833665b28b48a2bcb724164eba7b55c15f5467)
+    nb_q <- poly2nb(dt, queen=TRUE)
     w <- nb2listw(nb_q, style="W", zero.policy=TRUE)
   }
   return(w)
 }
 
-Run_wts('./data/Chicago_parcels_points.shp')
-Run_wts('./data/natregimes.shp')
-Run_wts('./data/NYC_Area2010_2data.shp')
-Run_wts('./data/us-sdoh-2014.shp')
+Run_wts('./data/Chicago_parcels_points.shp') # 20s
+Run_wts('./data/natregimes.shp') # 1s
+Run_wts('./data/NYC_Area2010_2data.shp') # 35s
+Run_wts('./data/us-sdoh-2014.shp') # 28s
 
